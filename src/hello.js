592a0e4ac410064d422ea296c76c5b561c165e3d
jayropatti <jayro.pattimaipau@student.hu.nl>
2022-05-11 13:02:06 +0200

    commit wijzigingen

--- src/hello.js
+++ src/hello.js
@@ -1,1 +1,13 @@
-console.log('Hello JavaScript, from hello.js');
+/* eslint-disable import/prefer-default-export */
+
+/**
+ *
+ * @function helloJS
+ * @returns <String> - returns the string 'Hello JavaScript'
+ */
+function helloJS() {
+}
+
+export {
+  helloJS,
+};
