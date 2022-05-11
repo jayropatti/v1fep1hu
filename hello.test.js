286661a421e41a5e22e6c24326243932cda607f1
jayropatti <jayro.pattimaipau@student.hu.nl>
2022-05-11 13:28:56 +0200

    commit

--- hello.test.js
+++ hello.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import { expect } from '@esm-bundle/chai';
 
-import { helloJS } from '../src/hello';
+import { helloJS } from './src/hello';
 
 describe('Test van de hello.js', () => {
   it('de functie helloJS zou de string "Hello JavaScript" terug moeten geven', () => {
